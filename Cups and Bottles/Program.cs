aa4048c169c2609a0a88f6b66a5dcdccb652af47
Dimo Todorov <baal98@gmail.com>
2022-06-23 00:11:45 +0300

    Add files via upload

---
+++ Cups and Bottles/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cupsCapacity = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            List<int> bottleCapacity = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            bottleCapacity.Reverse();
+
+            Queue<int> queueCups = new Queue<int>();
+            Queue<int> queueBottles = new Queue<int>();
+            
+            foreach (int cup in cupsCapacity)
+            {
+                queueCups.Enqueue(cup);
+            }
+            foreach (int bottle in bottleCapacity)
+            {
+                queueBottles.Enqueue(bottle);
+            }
+            int wastedWater = 0;
+            int currentBotlle = 0;
+            int currentCup = 0;
+
+            while (queueCups.Count > 0 && queueBottles.Count > 0)
+            {
+                if  (currentCup > 0)
+                {
+                    currentBotlle = queueBottles.Dequeue();
+                }
+                else
+                {
+                    currentBotlle = queueBottles.Dequeue();
+                    currentCup = queueCups.Peek();
+                }
+
+                if ((currentBotlle - currentCup) > 0)
+                {
+                    wastedWater += currentBotlle - currentCup;
+                    currentCup = 0;
+                    queueCups.Dequeue();
+                }
+                else if ((currentBotlle - currentCup) == 0)
+                {
+                    currentCup = 0;
+                    queueCups.Dequeue();
+                }
+                else
+                {
+                    currentCup -= currentBotlle;
+                }
+
+            }
+            if (queueBottles.Count > 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", queueBottles)}");
+            }
+            else if (queueCups.Count > 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", queueCups)}");
+            }
+            
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
---
+++ Fashion Boutique/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesInBox = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            Queue<int> queue = new Queue<int>();
+            foreach (int clothes in clothesInBox)
+            {
+                queue.Enqueue(clothes);
+            }
+
+            int counter = 0;
+            int temp = rackCapacity;
+            int queueCount = queue.Count;
+
+            while (queue.Count != 0)
+            {
+                if (rackCapacity - queue.Peek() < 0)
+                {
+                    counter++;
+                    rackCapacity = temp;
+                }
+                else if (queue.Count == 1)
+                {
+                    counter++;
+                    queue.Dequeue();
+                    break;
+                }
+                else
+                {
+                    rackCapacity -= queue.Dequeue();
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
