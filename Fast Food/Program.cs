e824cc4e9eda0db3452ce7479042f87699a1f5e0
Dimo Todorov <baal98@gmail.com>
2022-06-24 00:29:25 +0300

    Add files via upload

---
+++ Fast Food/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+
+            Queue<int> queue = new Queue<int>();
+
+            int[] orders = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            foreach (int order in orders)
+            {
+                queue.Enqueue(order);
+            }
+            Console.WriteLine(queue.Max());
+
+            while (queue.Count != 0)
+            {
+                if (queue.Peek() > foodQuantity)
+                {
+                    break;
+                }
+                foodQuantity -= queue.Dequeue();
+            }
+            if (queue.Count != 0)
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue.ToArray())}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
---
+++ Hot Potato/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split(' ');
+
+            int numberOfToss = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>();
+
+            foreach (string kid in kids)
+            {
+                queue.Enqueue(kid);
+            }
+            int tempToss = numberOfToss;
+
+            while (queue.Count != 1)
+            {
+                string removedKid = "";
+                numberOfToss = tempToss;
+                while (numberOfToss != 0)
+                {
+                    numberOfToss--;
+                    removedKid = queue.Peek();
+                    if (numberOfToss == 0)
+                    {
+                        break;
+                    }
+                    queue.Enqueue(queue.Dequeue());
+                }
+                queue.Dequeue();
+                Console.WriteLine($"Removed {removedKid}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
