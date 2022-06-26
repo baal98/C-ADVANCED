07ff87480e60e2645d21af4cd92b00d5732164b9
Dimo Todorov <baal98@gmail.com>
2022-06-26 17:47:50 +0300

    Add files via upload

---
+++ Matching Brackets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputs = Console.ReadLine();
+            int indexOfOpenBracket = 0;
+            int indexOfCloseBracket = 0;
+            Stack<int> stack = new Stack<int>();
+
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                if (inputs[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (inputs[i] == ')')
+                {
+                    indexOfOpenBracket = stack.Pop();
+                    indexOfCloseBracket = i;
+                    string findedBrackets = inputs.Substring(indexOfOpenBracket, indexOfCloseBracket - indexOfOpenBracket + 1);
+                    Console.WriteLine(findedBrackets);
+                }
+            }
+
+        }
+    }
+}
