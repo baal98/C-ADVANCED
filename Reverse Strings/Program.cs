0e298660bf6832cf787e8d7df5451a29a1b41054
Dimo Todorov <baal98@gmail.com>
2022-06-28 00:36:24 +0300

    Add files via upload

---
+++ Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack stack = new Stack();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
