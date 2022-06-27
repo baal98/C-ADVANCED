4bd2dba8f13559e2e42fe11d45b118f3453923ed
Dimo Todorov <baal98@gmail.com>
2022-06-27 08:35:13 +0300

    Add files via upload

---
+++ Maximum and Minimum Element/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            
+            while (numberOfInputs != 0)
+            {
+                string[] commands = Console.ReadLine().Split(' ');
+
+                string action = commands[0];
+
+                if (action == "1")
+                {
+                    int ellement = int.Parse(commands[1]);
+                    stack.Push(ellement);
+                }
+                else if (action == "2")
+                {
+                    if (!(stack.Count == 0))
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (action == "3")
+                {
+                    if (!(stack.Count == 0))
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                else if (action == "4")
+                {
+                    if (!(stack.Count == 0))
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+                numberOfInputs--;
+            }
+            Console.WriteLine(string.Join(", ", stack.ToArray()));
+        }
+    }
+}
