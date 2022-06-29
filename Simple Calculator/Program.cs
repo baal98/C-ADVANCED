833b16fecab00ebf66cc19fedc0bb6c0739282f7
Dimo Todorov <baal98@gmail.com>
2022-06-29 05:57:54 +0300

    Add files via upload

---
+++ Simple Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Reverse().ToArray();
+
+            Stack<string> stack = new Stack<string>();
+
+            foreach (string input in inputs)
+            {
+                stack.Push(input);
+            }
+            int stackLength = stack.Count;
+            int sum = int.Parse(stack.Pop());
+
+            for (int i = 0; i < stackLength; i++)
+            {
+                string input = stack.Pop();
+                if (input == "+")
+                {
+                    sum += int.Parse(stack.Pop());
+                }
+                else if (input == "-")
+                {
+                    sum -= int.Parse(stack.Pop());
+                }
+                if (stack.Count == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
