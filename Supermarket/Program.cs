41bc8592a67a5735ec152b1602399dd4f67d66ce
Dimo Todorov <baal98@gmail.com>
2022-07-01 00:26:21 +0300

    Add files via upload

---
+++ Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (command == "Paid")
+                {
+                    while (queue.Count != 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
