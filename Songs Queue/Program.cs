219f6627eee1362c45e5169560a542074ee272f0
Dimo Todorov <baal98@gmail.com>
2022-06-30 09:10:38 +0300

    Add files via upload

---
+++ Songs Queue/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ").ToArray();
+
+            Queue<string> queueSongs = new Queue<string>();
+
+            foreach (string s in songs)
+            {
+                queueSongs.Enqueue(s);
+            }
+
+            while (queueSongs.Count != 0)
+            {
+                string command = Console.ReadLine();
+
+                int indexOfSpace = command.IndexOf(' ');
+                if (indexOfSpace != -1)
+                {
+                    string action = command.Substring(0, indexOfSpace);
+                    string song = command.Substring(indexOfSpace + 1);
+
+                    if (queueSongs.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                        continue;
+                    }
+                    queueSongs.Enqueue(song);
+                }
+                else
+                {
+                    if (command == "Play")
+                    {
+                        queueSongs.Dequeue();
+                        continue;
+                    }
+                    Console.WriteLine(string.Join(", ", queueSongs.ToArray()));
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
