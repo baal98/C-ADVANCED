29d652d635872f2ffc8fcb43eac17802510c034d
Dimo Todorov <baal98@gmail.com>
2022-06-30 09:11:19 +0300

    Add files via upload

---
+++ Stack Sum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack stack = new Stack();
+
+            foreach (int digit in digits)
+            {
+                stack.Push(digit);
+            }
+            string command;
+
+            while ((command = Console.ReadLine().ToUpper()) != "END")
+            {
+                string[] inputs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = inputs[0];
+
+                if (action == "ADD")
+                {
+                    int firstDigit = int.Parse(inputs[1]);
+                    int secondDigit = int.Parse(inputs[2]);
+                    stack.Push(firstDigit);
+                    stack.Push(secondDigit);
+                }
+                else if (action == "REMOVE")
+                {
+                    int digitsToRemove = int.Parse(inputs[1]);
+
+                    if (digitsToRemove >= stack.Count)
+                    {
+                        continue;
+                    }
+                    for (int i = 0; i < digitsToRemove; i++)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+            int sumOfDigits = 0;
+
+            foreach (var digit in stack)
+            {
+                sumOfDigits += (int)digit;
+            }
+            Console.WriteLine($"Sum: {sumOfDigits}");
+        }
+    }
+}
